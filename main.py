454395d89c48e8839e7a901f934b8133f47e8bf0
Mohammad Khan <mohdahmedk@gmail.com>
2023-01-22 11:31:11 +0530

    Project completed with updated twilio API and location

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
+import os
+from twilio.rest import Client
 
 parameters = {
     "appid": "edc8e692ca16131a597b36e15a8c5260",
     "units": "metric",
     "exclude": "current,minutely,daily,alerts",
-    # "lat": "18.687100",
-    "lat": "49.314510",
-    # "lon": "73.830250"
-    "lon": "-122.855690"
+    "lat": "18.687100",
+    # "lat": "49.314510",      # For testing it is already raining location from the world
+    "lon": "73.830250"
+    # "lon": "-122.855690"     # For testing it is already raining location from the world
 }
 
 api_endpoint = "https://api.openweathermap.org/data/2.5/weather"
@@ -21,7 +23,7 @@ data = connection.json()
 
 for n in range(12):
     weather_id = data["hourly"][n]["weather"][0]["id"]
-    print(weather_id)
+    # print(weather_id)
     if weather_id < 700:
         it_is_raining = True
         break
@@ -30,8 +32,16 @@ for n in range(12):
 
 
 if it_is_raining:
-    print("Bring and umbrella")
-else:
-    print("There will be no rain Today.")
+    # Following environment variable can be find in twilio dashboard
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="bring an umbrella.",
+        from_=os.environ["MY_TWILIO_NUMBER"],  # Your twilio number can be found on twilio dashboard
+        to=os.environ['MY_NUMBER']             # Your personal number
+    )
+
+    print(message.status)
 
